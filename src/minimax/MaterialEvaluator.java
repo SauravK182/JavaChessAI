93a12170a248e85136d253f63c365fa9039cddfb
Marios Petrov <118226559+marios-petrov@users.noreply.github.com>
2023-05-04 22:44:19 -0400

    Update MaterialEvaluator.java
    Simplified lines 31-36 into one line, also adjusted comment alignments and contents to be more robust and succinct simultaneously.

--- src/minimax/MaterialEvaluator.java
+++ src/minimax/MaterialEvaluator.java
@@ -1,26 +1,21 @@
 package minimax;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.HashMap;
-
 import com.github.bhlangonijr.chesslib.Board;
 import com.github.bhlangonijr.chesslib.Side;
 
 /**
- * A simple evaluation class for chess pieces. Defines piece weight using the conventional
- * chess point system (mid-game) for simple material evaluation.
- * @author Saurav Kiri
+ * Chess piece evaluation class that implements the BoardEvaluator interface. 
+ * The classical chess point system (mid-game) is utilized for material evaluations/calculations.
+ * @authors Saurav Kiri and Marios Petrov
  * @since May 03, 2023
  */
 public class MaterialEvaluator implements BoardEvaluator {
 
     private static HashMap<String, Double> pieceWeight;
 
-    /* weights are defined as per Dan Heisman's book 'Everyone's 2nd Chess Book':
+    /* 
+     * weights are defined as per Dan Heisman's book 'Everyone's 2nd Chess Book':
      * https://www.chess.com/forum/view/general/whats-a-chess-piece-worth
      */
      static {
@@ -33,14 +28,7 @@ public class MaterialEvaluator implements BoardEvaluator {
         pieceWeight.put("k", -100d);
         
         // add white pieces - simply make chars upper case, add positive values
-        HashMap<String, Double> whitePieceWeights = new HashMap<>();
-        pieceWeight.forEach((key, value) -> {
-            double whitePieceWeight = (double) -1 * value;
-            whitePieceWeights.put(key.toUpperCase(), whitePieceWeight);
-        });
-
-        // merge maps - do not need to worry about overwriting keys
-        pieceWeight.putAll(whitePieceWeights);
+        pieceWeight.forEach((key, value) -> pieceWeight.put(key.toUpperCase(), -value));
     }
 
     /**
@@ -56,24 +44,30 @@ public class MaterialEvaluator implements BoardEvaluator {
      * point mappings defined in the {@code pieceWeight} instance variable.
      * @return A double indicating total piece value on the board.
      */
+    
     @Override
     public double evaluationScheme(Board board) {
         if (board.isMated()) {
+
             // if black is mated, white has won
             if (board.getSideToMove() == Side.BLACK) {
                 return 1e6d;
             } else {
+
             // else if white is mated, black has won
                 return -1e6d;
             }
+
             // if board is stalemated, return 0 as its a draw
         } else if (board.isStaleMate()) {
             return 0d;
         }
 
         double materialSum = 0;
+
         for (char position: board.toString().replaceAll("Side:.*", "").toCharArray()) {
             String piece = Character.toString(position);
+
             if (pieceWeight.containsKey(piece)) {
                 materialSum+=pieceWeight.get(piece);
             }
