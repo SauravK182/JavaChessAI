ebbde70a20a505908e9723c30ed59947b5ce1f61
Marios Petrov <118226559+marios-petrov@users.noreply.github.com>
2023-05-06 17:55:05 -0400

    Update SimpleMinimaxTest.java

--- src/test/SimpleMinimaxTest.java
+++ src/test/SimpleMinimaxTest.java
@@ -13,7 +13,8 @@ import minimax.MaterialEvaluator;
  * @author Marios Petrov
  * @since 05/06/2023
  */
-public class SimpleMinimaxTest {
+public class SimpleMinimaxTest 
+{
 
     /**
      * Main method to run the SimpleMinimax test.
@@ -21,7 +22,8 @@ public class SimpleMinimaxTest {
      * @param args command line arguments
      * @throws InterruptedException if the sleep operation is interrupted
      */
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException 
+    {
         SimpleMinimaxTest test = new SimpleMinimaxTest();
         test.testSimpleMinimax();
     }
@@ -31,33 +33,45 @@ public class SimpleMinimaxTest {
      *
      * @throws InterruptedException if the sleep operation is interrupted
      */
-    public static void testSimpleMinimax() throws InterruptedException {
+    public static void testSimpleMinimax() throws InterruptedException 
+    {
         Board board = new Board();
         SimpleMinimax minimax = new SimpleMinimax(4, new MaterialEvaluator());
 
         // Play 10 moves for each side
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) 
+        {
             System.out.println("Move #" + (2 * i + 1));
-            try {
+            
+            try 
+            {
                 Move whiteMove = minimax.findBestMove(board);
                 board.doMove(whiteMove);
                 gui.BoardGUI.drawBoard(board);
                 Thread.sleep(2000);
                 System.out.println("White move: " + whiteMove);
                 System.out.println(board.toString());
-            } catch (MoveException e) {
+            } 
+            
+            catch (MoveException e) 
+            {
                 System.out.println("Error executing white move: " + e.getMessage());
             }
 
             System.out.println("Move #" + (2 * i + 2));
-            try {
+            
+            try 
+            {
                 Move blackMove = minimax.findBestMove(board);
                 board.doMove(blackMove);
                 gui.BoardGUI.drawBoard(board);
                 Thread.sleep(2000);
                 System.out.println("Black move: " + blackMove);
                 System.out.println(board.toString());
-            } catch (MoveException e) {
+            } 
+            
+            catch (MoveException e) 
+            {
                 System.out.println("Error executing black move: " + e.getMessage());
             }
         }
