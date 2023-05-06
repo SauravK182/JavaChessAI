60675470c61b99a2b42295a423212397332f7b3c
Marios Petrov <118226559+marios-petrov@users.noreply.github.com>
2023-05-06 17:41:30 -0400

    Tests for AdvancedMinimax

---
+++ src/test/AdvancedMinimaxTest.java
@@ -0,0 +1,53 @@
+package test;
+
+import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.move.Move;
+import com.github.bhlangonijr.chesslib.move.MoveException;
+
+import gui.BoardGUI;
+import minimax.AdvancedMinimax;
+import minimax.MaterialEvaluator;
+
+/**
+ * Test class for AdvancedMinimax AI.
+ * 
+ * @author Marios Petrov
+ * @since 05/06/2023
+ */
+public class AdvancedMinimaxTest {
+
+    /**
+     * Main method to run the AdvancedMinimax test.
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        Board board = new Board();
+        AdvancedMinimax minimax = new AdvancedMinimax(4, new MaterialEvaluator());
+
+        // Play 10 moves for each side
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Move #" + (2 * i + 1));
+            try {
+                Move whiteMove = minimax.findBestMove(board);
+                board.doMove(whiteMove);
+                BoardGUI.drawBoard(board);
+                System.out.println("White move: " + whiteMove);
+                System.out.println(board.toString());
+            } catch (MoveException e) {
+                System.out.println("Error executing white move: " + e.getMessage());
+            }
+
+            System.out.println("Move #" + (2 * i + 2));
+            try {
+                Move blackMove = minimax.findBestMove(board);
+                board.doMove(blackMove);
+                BoardGUI.drawBoard(board);
+                System.out.println("Black move: " + blackMove);
+                System.out.println(board.toString());
+            } catch (MoveException e) {
+                System.out.println("Error executing black move: " + e.getMessage());
+            }
+        }
+    }
+}
