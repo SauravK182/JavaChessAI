d5ee32d047ce4d1f53364371971f1984d050ea82
Marios Petrov <118226559+marios-petrov@users.noreply.github.com>
2023-05-06 17:46:50 -0400

    Added documentation and used inheritance

--- src/minimax/AdvancedMinimax.java
+++ src/minimax/AdvancedMinimax.java
@@ -6,13 +6,32 @@ import com.github.bhlangonijr.chesslib.Side;
 
 import java.lang.Math;
 
+/**
+ * The AdvancedMinimax class implements the Strategy interface and extends SimpleMinix. AdvancedMinimax provides
+ * an implementation for the alpha-beta pruning algorithm. It also provides a numericalMoveCalculator method for recursively calculating the score
+ * of a given board state to a specified depth, and a quiescenceSearch method to recursively
+ * search for the optimal score when only captures and checks are possible.
+ * @author  Marios Petrov
+ * @since May 06, 2023
+ */
 
 public class AdvancedMinimax extends SimpleMinimax {
 
+    /**
+     * Constructs an AdvancedMinimax object with a depth of 4 and a MaterialEvaluator object
+     * for evaluating board states.
+     */
+    
     public AdvancedMinimax() {
         super();
     }
 
+    /**
+     * Constructs an AdvancedMinimax object with the specified depth and a MaterialEvaluator
+     * object for evaluating board states.
+     * @param depth The maximum depth to search.
+     */
+    
     public AdvancedMinimax(int depth) {
         super(depth);
     }
@@ -42,6 +61,14 @@ public class AdvancedMinimax extends SimpleMinimax {
         return optimalMove;
     }
 
+    /**
+     * Recursively calculates the score of a given board state to the specified depth
+     * using the minimax algorithm.
+     * @param board The board state to evaluate.
+     * @param depth The maximum depth to search.
+     * @return The score of the board state.
+     */
+    
     private double alphaBeta(Board board, int depth, double alpha, double beta, boolean isMaximizingPlayer) {
         if (depth == 0) {
             return quiescenceSearch(board, alpha, beta, isMaximizingPlayer);
