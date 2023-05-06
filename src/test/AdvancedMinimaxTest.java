d090baf57bda2ec0315d3fe2905eb24b393e14f5
Marios Petrov <118226559+marios-petrov@users.noreply.github.com>
2023-05-06 17:53:14 -0400

    Update AdvancedMinimaxTest.java

--- src/test/AdvancedMinimaxTest.java
+++ src/test/AdvancedMinimaxTest.java
@@ -14,38 +14,51 @@ import minimax.MaterialEvaluator;
  * @author Marios Petrov
  * @since 05/06/2023
  */
-public class AdvancedMinimaxTest {
+public class AdvancedMinimaxTest 
+{
 
     /**
      * Main method to run the AdvancedMinimax test.
      * 
      * @param args command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Board board = new Board();
         AdvancedMinimax minimax = new AdvancedMinimax(4, new MaterialEvaluator());
 
         // Play 10 moves for each side
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) 
+        {
             System.out.println("Move #" + (2 * i + 1));
-            try {
+            
+            try 
+            {
                 Move whiteMove = minimax.findBestMove(board);
                 board.doMove(whiteMove);
                 BoardGUI.drawBoard(board);
                 System.out.println("White move: " + whiteMove);
                 System.out.println(board.toString());
-            } catch (MoveException e) {
+            } 
+            
+            catch (MoveException e) 
+            {
                 System.out.println("Error executing white move: " + e.getMessage());
             }
 
             System.out.println("Move #" + (2 * i + 2));
-            try {
+           
+            try 
+            {
                 Move blackMove = minimax.findBestMove(board);
                 board.doMove(blackMove);
                 BoardGUI.drawBoard(board);
                 System.out.println("Black move: " + blackMove);
                 System.out.println(board.toString());
-            } catch (MoveException e) {
+            } 
+            
+            catch (MoveException e) 
+            {
                 System.out.println("Error executing black move: " + e.getMessage());
             }
         }
