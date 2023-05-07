9a3b9ad19bedc4bcdfce987be8561f32e4e97601
SauravK182 <99155518+SauravK182@users.noreply.github.com>
2023-05-06 21:45:45 -0400

    add more tests for minimax - check found move for non-mate

--- src/test/TestMinimax.java
+++ src/test/TestMinimax.java
@@ -89,11 +89,55 @@ public class TestMinimax {
         board.undoMove();
         Assertions.assertFalse(board.isMated());
 
+        /*
         start = System.nanoTime();
         board.doMove(advMini.findBestMove(board));
         stop = System.nanoTime();
         time = (stop - start) / 1e9;
         System.out.println("Seconds taken for advanced minimax: " + String.format("%, .2f", time));
         Assertions.assertTrue(board.isMated());
+        */
+    }
+
+    /**
+     * Check for optimal move at depth of 1
+     */
+    @Test
+    void checkOptimalMove() {
+        SimpleMinimax mini2 = new SimpleMinimax(1);
+        AdvancedMinimax mini3 = new AdvancedMinimax(1);
+
+        /* here, best move should be queen capture c5 at a depth of 1,
+         * since this yields the most material for white.
+         */
+        String fen = "5k2/8/8/2qn4/2bQr3/8/8/3K4 w - - 0 1";
+        board.loadFromFen(fen);
+        String miniMove = mini2.findBestMove(board).toString();
+        Assertions.assertEquals(miniMove, "d4c5");
+
+        String miniMoveAdv = mini3.findBestMove(board).toString();
+        Assertions.assertEquals(miniMoveAdv, "d4c5");
+    }
+
+    /**
+     * Check for optimal move at depth 2, using contrived example
+     */
+    @Test
+    void checkMoveDepthTwo() {
+        SimpleMinimax mini2 = new SimpleMinimax(2);
+        AdvancedMinimax mini3 = new AdvancedMinimax(2);
+
+        /*
+         * Contrived example - here, best move should be
+         * queen captures rook g1 at depth 2
+         */
+        String fen = "5k1r/q7/8/8/3Q4/8/8/3K2r1 w - - 0 1";
+        board.loadFromFen(fen);
+        String miniMove = mini2.findBestMove(board).toString();
+        Assertions.assertEquals(miniMove, "d4g1");
+
+        String miniMoveAdv = mini3.findBestMove(board).toString();
+        Assertions.assertEquals(miniMoveAdv, "d4g1");
+
     }
 }
